b6184d182116a51f001c83b973ec0bcb73c771fe
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-24 21:54:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let p=document.createElement("p");
+p.id="timer";
+
+const now = new Date();
+const currentDateTime = now.toLocaleString();
+
+
+document.body.append(p);
+
+p.textContent=currentDateTime;
+setInterval(currentDateTime,1000);
+
+
+
